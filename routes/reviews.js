eb377010056c117a503ba9c534f73200dd6290a1
Witchapat Jindanath <jwitchapat@gmail.com>
2024-03-26 00:42:56 +0700

    routes reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,10 +4,10 @@ const router = express.Router({mergeParams: true});
 const {protect, authorize} = require('../middleware/auth');
 
 router.route('/')
-    .get(protect, getReviews)
+    .get(getReviews)
     .post(protect, authorize('admin', 'user'), createReview);
 router.route('/:id')
-    .get(protect, getReview)
+    .get(getReview)
     .put(protect, authorize('admin', 'user'), updateReview)
     .delete(protect, authorize('admin', 'user'), deleteReview);
 
